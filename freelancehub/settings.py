58ff461dfa09446b13946044aaf48a185b5c7a9c
DavidcD8 <davidcd08@gmail.com>
2025-05-26 11:11:37 +0100

    Fix debug in env and settings set to false

--- freelancehub/settings.py
+++ freelancehub/settings.py
@@ -15,7 +15,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Core settings
 SECRET_KEY = env('SECRET_KEY')
-DEBUG = True
+DEBUG = env('DEBUG')
 
 # Allowed hosts
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['127.0.0.1', 'localhost'])
